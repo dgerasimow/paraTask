dcc66ad0ee92ff61ac32eed8bf465e300ad090ba
thw1zard <68274539+thw1zard@users.noreply.github.com>
2021-11-02 18:52:39 +0300

    Initial Commit

---
+++ taskWithUserDefaults/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskWithUserDefaults
+//
+//  Created by Danil Gerasimov on 02.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
